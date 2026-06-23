43869c4ea771dbb48d7fea1447e328087e5a23f2
Hansiwursti <BSeiler@cdtnue.onmicrosoft.com>
2026-06-23 12:16:02 +0200

    init commit

---
+++ Musiker/daten.sql
@@ -0,0 +1,13 @@
+CREATE TABLE genre (
+g_id int identity(1,1) primary key,
+genre varchar(30)
+);
+
+insert into genre values
+('Rock'),
+('Pop'),
+('Jazz'),
+('Hip-Hop'),
+('Klassik'),
+('Electronic'),
+('Reggae');
