be2eb81e84eaa95f366cd55a8a64197219194e89
Hansiwursti <BSeiler@cdtnue.onmicrosoft.com>
2026-06-24 14:05:27 +0200

    erweitert ver_ort

--- Musiker/Musiker.sql
+++ Musiker/Musiker.sql
@@ -22,3 +22,16 @@ join interpret on titel.i_id = interpret.i_id
 join genre on titel.g_id = genre.g_id
 
 
+create table veranstaltungsort (
+v_id int identity(1,1) primary key,
+ort_name varchar(100)
+);
+
+create table in_ver (
+datum_von date,
+datum_bis date, 
+i_id int,
+v_id int not null,
+primary key (i_id, datum_von, datum_bis),
+foreign key (v_id) references veranstaltungsort(v_id)
+);
